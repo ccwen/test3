5379ead833f943c606fdeb6f936eca2bf7e79b6b
yapcheahshen <yapcheahshen@gmail.com>
2015-04-08 12:11:09 +0800

    test credential

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 3,
+ "title": "test3",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/ksanaforge/test3/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  64,
+  632494,
+  1015954,
+  277980,
+  769,
+  35601,
+  113498
+ ],
+ "filedates": [
+  "2015-03-16T17:06:42.491Z",
+  "2015-03-16T17:06:42.489Z",
+  "2015-03-16T17:06:42.617Z",
+  "2015-04-08T03:49:14.925Z",
+  "2015-03-16T17:06:42.531Z",
+  "2015-03-16T17:06:48.578Z",
+  "2015-03-16T17:06:42.482Z",
+  "2015-03-16T17:06:42.463Z"
+ ],
+ "date": "2015-04-08T03:49:15.831Z"
+})
